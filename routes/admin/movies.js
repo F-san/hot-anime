da96a3ac758515eb023a07515a198fa00f16e12c
F-san <635660264@qq.com>
2020-12-18 18:48:58 +0800

    影片前台展示和新增功能

--- routes/admin/movies.js
+++ routes/admin/movies.js
@@ -1,10 +1,27 @@
 const router = require('express').Router();
+const Movie = require('../../models/movie');
+
+// 数据库展示到影片信息页面中
+router.get('/',async (req, res) => {
+    const data = await Movie.find().sort({_id:-1});
+    res.render("admin/movies/index", {
+        data
+    })
+})
 
 router.get('/', (req, res) => {
     res.render("admin/movies/index");
 })
 router.get('/new', (req, res) => {
+    // 返回一个view渲染之后得到的HTML文本
     res.render("admin/movies/new");
 })
 
+// 新增影片
+router.post('/create',async (req, res) => {
+    const movie = new Movie(req.body);
+    await movie.save();
+    // 重定向到影片列表页
+    res.redirect('/admin/movies');
+})
 module.exports = router;
